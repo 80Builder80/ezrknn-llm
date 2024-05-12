dc01c62f9499418aeb2ba74d37e7ab2a2957a416
80Builder80 <85843888+80Builder80@users.noreply.github.com>
2024-05-11 22:01:50 -0500

    Translated gradio_server.py

--- rkllm-runtime/examples/rkllm_server_demo/rkllm_server/gradio_server.py
+++ rkllm-runtime/examples/rkllm_server_demo/rkllm_server/gradio_server.py
@@ -8,19 +8,19 @@ import time
 import gradio as gr
 import argparse
 
-# 设定环境变量
+# Set environment variables
 os.environ["GRADIO_SERVER_NAME"] = "0.0.0.0"
 os.environ["GRADIO_SERVER_PORT"] = "8080"
 
-# 设置动态库路径
+# Set the dynamic library path
 rkllm_lib = ctypes.CDLL('lib/librkllmrt.so')
 
-# 定义全局变量，用于保存回调函数的输出，便于在gradio界面中输出
+# Define global variables to save callback function outputs for display in the gradio interface
 global_text = []
 global_state = -1
-split_byte_data = bytes(b"") # 用于保存分割的字节数据
+split_byte_data = bytes(b"")  # Used to store split byte data
 
-# 定义动态库中的结构体
+# Define a structure in the dynamic library
 class Token(ctypes.Structure):
     _fields_ = [
         ("logprob", ctypes.c_float),
@@ -34,13 +34,13 @@ class RKLLMResult(ctypes.Structure):
         ("num", ctypes.c_int32)
     ]
 
-# 定义回调函数
+# Define a callback function
 def callback(result, userdata, state):
     global global_text, global_state, split_byte_data
     if state == 0:
-        # 保存输出的token文本及RKLLM运行状态
+        # Save the token text output and RKLLM running state
         global_state = state
-        # 需要监控当前的字节数据是否完整，不完整则进行记录，后续进行解析
+        # Monitor if the current byte data is complete; if not, record it for later parsing
         try:
             global_text.append((split_byte_data + result.contents.text).decode('utf-8'))
             print((split_byte_data + result.contents.text).decode('utf-8'), end='')
@@ -49,18 +49,18 @@ def callback(result, userdata, state):
             split_byte_data += result.contents.text
         sys.stdout.flush()
     elif state == 1:
-        # 保存RKLLM运行状态
+        # Save the RKLLM running state
         global_state = state
         print("\n")
         sys.stdout.flush()
     else:
         print("run error")
 
-# Python端与C++端的回调函数连接
+# Connect the Python-side to the C++-side callback function
 callback_type = ctypes.CFUNCTYPE(None, ctypes.POINTER(RKLLMResult), ctypes.c_void_p, ctypes.c_int)
 c_callback = callback_type(callback)
 
-# 定义动态库中的结构体
+# Define a structure in the dynamic library for RKLLM model parameters
 class RKNNllmParam(ctypes.Structure):
     _fields_ = [
         ("model_path", ctypes.c_char_p),
@@ -81,16 +81,16 @@ class RKNNllmParam(ctypes.Structure):
         ("use_gpu", ctypes.c_bool)
     ]
 
-# 定义RKLLM_Handle_t和userdata
+# Define RKLLM_Handle_t and userdata
 RKLLM_Handle_t = ctypes.c_void_p
 userdata = ctypes.c_void_p(None)
 
-# 设置提示文本
-PROMPT_TEXT_PREFIX = "<|im_start|>system You are a helpful assistant. <|im_end|> <|im_start|>user"
-PROMPT_TEXT_POSTFIX = "<|im_end|><|im_start|>assistant"
+# Set prompt text
+PROMPT_TEXT_PREFIX = "system You are a helpful assistant.  user"
+PROMPT_TEXT_POSTFIX = "assistant"
 
-# 定义Python端的RKLLM类，其中包括了对动态库中RKLLM模型的初始化、推理及释放操作
-class RKLLM(object):
+# Define the RKLLM class on the Python side, including initialization, inference, and release operations for the RKLLM model in the dynamic library
+class RKLLM:
     def __init__(self, model_path, target_platform):
         rknnllm_param = RKNNllmParam()
         rknnllm_param.model_path = bytes(model_path, 'utf-8')
@@ -137,92 +137,92 @@ class RKLLM(object):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--target_platform', help='目标平台: 如rk3588/rk3576;')
-    parser.add_argument('--rkllm_model_path', help='Linux板端上已转换好的rkllm模型的绝对路径')
+    parser.add_argument('--target_platform', help='Target platform: e.g., rk3588/rk3576;')
+    parser.add_argument('--rkllm_model_path', help='Absolute path of the converted rkllm model on the Linux board')
     args = parser.parse_args()
 
     if not (args.target_platform in ["rk3588", "rk3576"]):
-        print("====== Error: 请指定正确的目标平台: rk3588/rk3576 ======")
+        print("====== Error: Please specify the correct target platform: rk3588/rk3576 ======")
         sys.stdout.flush()
         exit()
 
     if not os.path.exists(args.rkllm_model_path):
-        print("====== Error: 请给出准确的rkllm模型路径，需注意是板端的绝对路径 ======")
+        print("====== Error: Please provide the accurate path to the rkllm model, ensuring it is the absolute path on the board ======")
         sys.stdout.flush()
         exit()
 
-    # 定频设置
+    # Frequency setting
     command = "sudo bash fix_freq_{}.sh".format(args.target_platform)
     subprocess.run(command, shell=True)
 
-    # 设置文件描述符限制
+    # Set file descriptor limit
     resource.setrlimit(resource.RLIMIT_NOFILE, (102400, 102400))
 
-    # 初始化RKLLM模型
+    # Initialize the RKLLM model
     print("=========init....===========")
     sys.stdout.flush()
     target_platform = args.target_platform
     model_path = args.rkllm_model_path
     rkllm_model = RKLLM(model_path, target_platform)
-    print("RKLLM初始化成功！")
+    print("RKLLM initialization successful!")
     print("==============================")
     sys.stdout.flush()
 
-    # 记录用户输入的prompt         
+    # Record user input prompt         
     def get_user_input(user_message, history):
         history = history + [[user_message, None]]
         return "", history
 
-    # 获取RKLLM模型的输出并进行流式打印
+    # Get RKLLM model output and perform streaming printing
     def get_RKLLM_output(history):
-        # 链接全局变量，获取回调函数的输出信息
+        # Link global variables, get output information from the callback function
         global global_text, global_state
         global_text = []
         global_state = -1
 
-        # 创建模型推理的线程
+        # Create a thread for model inference
         model_thread = threading.Thread(target=rkllm_model.run, args=(history[-1][0],))
         model_thread.start()
 
-        # history[-1][1]表示当前的输出对话
+        # history[-1][1] represents the current output conversation
         history[-1][1] = ""
         
-        # 等待模型运行完成，定时检查模型的推理线程
+        # Wait for the model to complete, periodically check the inference thread
         model_thread_finished = False
         while not model_thread_finished:
             while len(global_text) > 0:
                 history[-1][1] += global_text.pop(0)
                 time.sleep(0.005)
-                # gradio在调用then方法式自动将yield返回的结果推进行输出
+                # Gradio automatically pushes yield returned results for output when the then method is called
                 yield history
 
             model_thread.join(timeout=0.005)
             model_thread_finished = not model_thread.is_alive()
 
-    # 创建gradio界面
+    # Create a gradio interface
     with gr.Blocks(title="Chat with RKLLM") as chatRKLLM:
         gr.Markdown("<div align='center'><font size='70'> Chat with RKLLM </font></div>")
-        gr.Markdown("### 在 inputTextBox 输入您的问题，按下 Enter 键，即可与 RKLLM 模型进行对话。")
-        # 创建一个Chatbot组件，用于显示对话历史
+        gr.Markdown("### Type your question in the inputTextBox, press Enter, and chat with the RKLLM model.")
+        # Create a Chatbot component to display conversation history
         rkllmServer = gr.Chatbot(height=600)
-        # 创建一个Textbox组件，让用户输入消息
+        # Create a Textbox component for user input
         msg = gr.Textbox(placeholder="Please input your question here...", label="inputTextBox")
-        # 创建一个Button组件，用于清除聊天历史
-        clear = gr.Button("清除")
+        # Create a Button component to clear chat history
+        clear = gr.Button("Clear")
 
-        # 将用户输入的消息提交给get_user_input函数，并且立即更新聊天历史
-        # 然后调用get_RKLLM_output函数，进一步更新聊天历史
-        # queue=False参数确保这些更新不会被放入队列，而是立即执行
+        # Submit user input to the get_user_input function and immediately update the chat history
+        # Then call the get_RKLLM_output function to further update the chat history
+        # The queue=False parameter ensures that these updates are executed immediately, not queued
         msg.submit(get_user_input, [msg, rkllmServer], [msg, rkllmServer], queue=False).then(get_RKLLM_output, rkllmServer, rkllmServer)
-        # 当点击清除按钮时，执行一个空操作（lambda: None），并且立即清除聊天历史
+        # When the clear button is clicked, perform a no-op (lambda: None) and immediately clear the chat history
         clear.click(lambda: None, None, rkllmServer, queue=False)
 
-    # 启用事件队列系统
+    # Enable the event queue system
     chatRKLLM.queue()
-    # 启动Gradio应用程序
+    # Launch the Gradio application
     chatRKLLM.launch()
 
     print("====================")
-    print("RKLLM模型推理结束, 释放RKLLM模型资源...")
+    print("RKLLM model inference complete, releasing RKLLM model resources...")
     rkllm_model.release()
     print("====================")
