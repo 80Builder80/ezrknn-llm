b7e2a95945452bcc506ba95a9525b1f1fecf7ad6
80Builder80 <85843888+80Builder80@users.noreply.github.com>
2024-05-11 21:44:18 -0500

    Update setup.sh

--- docker/setup.sh
+++ docker/setup.sh
@@ -5,15 +5,15 @@ apt update
 apt install -y python3 pip git curl wget nano sudo apt-utils
 
 # Better than using the default from Rockchip
-git clone https://github.com/Pelochus/ezrknn-llm/
-curl https://raw.githubusercontent.com/Pelochus/ezrknn-llm/master/install.sh | sudo bash
+git clone https://github.com/80Builder80/ezrknn-llm/
+curl https://raw.githubusercontent.com/80Builder80/ezrknn-llm/master/install.sh | sudo bash
 
 # For running the test.py
 pip install /ezrknn-llm/rkllm-toolkit/packages/rkllm_toolkit-1.0.1-cp38-cp38-linux_x86_64.whl
 
 # Clone some compatible LLMs
 cd /ezrknn-llm/rkllm-toolkit/examples/huggingface
-git clone https://huggingface.co/Qwen/Qwen-1_8B-Chat
+git clone https://huggingface.co/microsoft/Phi-3-mini-4k-instruct
 
 # Done here to avoid cloning full repository for the Docker image
 apt install -y git-lfs
