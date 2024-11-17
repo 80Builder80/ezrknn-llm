7ab1f9293d83fee75e1d1cd421a801d3c80cbdbe
80Builder80 <85843888+80Builder80@users.noreply.github.com>
2024-11-17 09:58:41 -0600

    Update entrypoint.sh
    make model directory and set permissions

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,8 +3,17 @@
 # Always useful
 apt update && apt full-upgrade -y
 
+# Ensure models directory exists
+if [ ! -d /models ]; then
+    mkdir -p /models
+    chmod -R 777 /models  # Ensure permissions
+    echo "Created /models directory with appropriate permissions."
+fi
+
 # Update repo without updating image
 cd /ezrknn-llm
 git pull
 
+# Start a bash session for interaction
 bash
+
